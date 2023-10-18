32283388acc16e4a6950c9d41d1d88b16af77398
SundayMba <sundinoh@gmail.com>
2023-10-18 19:45:02 +0100

    my code

--- main.c
+++ main.c
@@ -1,46 +1,140 @@
 #include "monty.h"
 #include <stdio.h>
-bus_t bus = {NULL, NULL, NULL, 0};
-/**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
-*/
-int main(int argc, char *argv[])
-{
-	char *content;
-	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
+content_t infoTray = {NULL, NULL, NULL};
 
+/**
+ * main - entry point of the program
+ * @argv: a vector of the input argument
+ * @argc: a count of the argument
+ * Return: 0 - success or EXIT_FAILURE
+ */
+
+int main(int argc, char **argv)
+{
+	int line_number = 0;
+	char opcode[SIZE], *line, **line_tokens;
+	FILE *file;
+	stack_t *stack = NULL;
+
+	/* check for valid item argument and handle errors */
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file\n");
+		fprintf(stderr, "%s\n", "USAGE: monty file");
 		exit(EXIT_FAILURE);
 	}
+	/* open the monty bytecode file for reading and handle errors */
 	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
+	if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while (read_line > 0)
+	line = fgets(opcode, SIZE, file);
+	infoTray.file = file;
+	infoTray.stack = &stack;
+	while (feof(file) == 0 && line != NULL)
 	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
-		if (read_line > 0)
-		{
-			execute(content, &stack, counter, file);
-		}
-		free(content);
+		line_number++;
+		/* tokenize the string line */
+		line_tokens = _strtok(line);
+		infoTray.token = line_tokens;
+		/* printTokens(line_tokens); */
+		if (line_tokens != NULL)
+			process_op(line_tokens, line_number, &stack, file);
+		line = fgets(opcode, SIZE, file);
 	}
-	free_stack(stack);
 	fclose(file);
-return (0);
+	freeStack(&stack);
+	return (0);
+}
+
+/**
+ * _strtok - tokenize the line of string
+ * @line: line of string
+ * Return: pointer to array of string tokens or NULL
+ */
+
+char **_strtok(char *line)
+{
+	char *sep, *token, **line_token;
+	int token_count;
+
+	sep = " \n\t";
+	if (line == NULL)
+		return (NULL);
+	token_count = tokCount(_strdup(line), sep);
+	if (token_count <= 0)
+		return (NULL);
+	line_token = (char **)malloc(sizeof(char *) * (token_count + 1));
+	if (line_token == NULL)
+		return (NULL);
+	token = strtok(line, sep);
+	token_count = 0;
+	while (token != NULL)
+	{
+		line_token[token_count++] = _strdup(token);
+		token = strtok(NULL, sep);
+	}
+	line_token[token_count] = NULL;
+	return (line_token);
+}
+
+/**
+ * tokCount - count the number of tokens
+ * @line: line of string
+ * @sep: seperators
+ * Return: number of tokens or -1
+ */
+
+int tokCount(char *line, char *sep)
+{
+	int line_count;
+	char *token;
+
+	line_count = 0;
+	token = strtok(line, sep);
+	while (token != NULL)
+	{
+		line_count++;
+		token = strtok(NULL, sep);
+	}
+	free(line);
+	return (line_count);
+}
+
+/**
+ * freeMemory - free a doubled pointer memory
+ * @tokens: pointer to an array of tokens
+ * Return: void
+ */
+
+void freeMemory(char **tokens)
+{
+	int i;
+
+	if (tokens != NULL)
+	{
+		for (i = 0; tokens[i]; i++)
+		{
+			if (tokens[i] != NULL)
+				free(tokens[i]);
+		}
+		free(tokens);
+	}
+}
+
+/**
+ * printTokens - print tokens
+ * @tokens: tokens to print
+ * Return: void
+ */
+
+void printTokens(char **tokens)
+{
+	if (tokens != NULL)
+	{
+		while (*tokens)
+			printf("%s ", *tokens++);
+		printf("\n");
+	}
 }
--- monty.h
+++ monty.h
@@ -1,5 +1,6 @@
 #ifndef MONTY_H
 #define MONTY_H
+
 #define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,6 +9,8 @@
 #include <fcntl.h>
 #include <string.h>
 #include <ctype.h>
+
+#define SIZE 1024
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -15,7 +18,7 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
@@ -23,57 +26,54 @@ typedef struct stack_s
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
-/**
- * struct bus_s - variables -args, file, line content
- * @arg: value
- * @file: pointer to monty file
- * @content: line content
- * @lifi: flag change stack <-> queue
- * Description: carries values through the program
- */
-typedef struct bus_s
-{
-	char *arg;
-	FILE *file;
-	char *content;
-	int lifi;
-}  bus_t;
-extern bus_t bus;
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
-ssize_t getstdin(char **lineptr, int file);
-char  *clean_line(char *content);
-void tlcpush(stack_t **head, unsigned int number);
-void tlcpall(stack_t **head, unsigned int number);
-void tlcpint(stack_t **head, unsigned int number);
-int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
-void free_stack(stack_t *head);
-void tlcpop(stack_t **head, unsigned int counter);
-void tlcswap(stack_t **head, unsigned int counter);
-void tlcadd(stack_t **head, unsigned int counter);
-void tlcnop(stack_t **head, unsigned int counter);
-void tlcsub(stack_t **head, unsigned int counter);
-void tlcdiv(stack_t **head, unsigned int counter);
-void tlcmul(stack_t **head, unsigned int counter);
-void tlcmod(stack_t **head, unsigned int counter);
-void tlcpchar(stack_t **head, unsigned int counter);
-void tlcpstr(stack_t **head, unsigned int counter);
-void tlcrotl(stack_t **head, unsigned int counter);
-void tlcrotr(stack_t **head, __attribute__((unused)) unsigned int counter);
-void addnode(stack_t **head, int n);
-void addqueue(stack_t **head, int n);
-void tlcqueue(stack_t **head, unsigned int counter);
-void tlcstack(stack_t **head, unsigned int counter);
+
+/**
+ * struct content_s - structure of content that will be used globally
+ * @token: array of tokens
+ * @stack: stack frame of nodes of opcode
+ * @file: open file
+ */
+
+typedef struct content_s
+{
+	char **token;
+	stack_t **stack;
+	FILE *file;
+} content_t;
+
+extern content_t infoTray;
+
+char **_strtok(char *line);
+int tokCount(char *line, char *sep);
+void push(stack_t **stack, int data);
+int check_valid_int(char *str);
+void process_op(char **tokens, unsigned int line, stack_t **stack, FILE *);
+void swap(stack_t **stack, unsigned int line);
+void pop(stack_t **stack, unsigned int line);
+void pint(stack_t **stack, unsigned int line);
+void pall(stack_t **stack, unsigned int line);
+void push(stack_t **stack, int n);
+int stack_len(stack_t *stack);
+void nop(stack_t **stack, unsigned int line);
+void add(stack_t **stack, unsigned int line);
+void printTokens(char **tokens);
+void freeMemory(char **tokens);
+char *_strdup(char *src);
+void freeStack(stack_t **stack);
+void pclean(char **, FILE *, stack_t **);
+
 #endif
